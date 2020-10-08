f002c272d7f0900a1033bb73e52085cd25defba6
pengbo.yang <obpt@sina.com>
2020-10-08 21:56:05 +0900

    migrate remote to core project

---
+++ src/YS.Time.Core/Remote/RemoteTimeServiceBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace YS.Time
+{
+    public abstract class RemoteTimeServiceBase : ITimeService
+    {
+        public RemoteTimeServiceBase(IOptions<RemoteTimeOptions> remoteOptions)
+        {
+            _ = remoteOptions ?? throw new ArgumentNullException(nameof(remoteOptions));
+            this.cacheTime = TimeSpan.FromSeconds(remoteOptions.Value.CacheSeconds);
+        }
+        private TimeSpan cacheTime;
+        private DateTimeOffset lastLocalTime;
+        private DateTimeOffset lastRemoteTime;
+        public async Task<DateTimeOffset> Current()
+        {
+            var localNow = DateTimeOffset.Now;
+            if (localNow - lastLocalTime > cacheTime)
+            {
+                lastRemoteTime = await OnGetRemoteTime();
+                lastLocalTime = localNow;
+            }
+            return lastRemoteTime + (localNow - lastLocalTime);
+        }
+
+        protected abstract Task<DateTimeOffset> OnGetRemoteTime();
+
+    }
+}
